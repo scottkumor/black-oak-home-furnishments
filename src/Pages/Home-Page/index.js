81949c54be17fe029d2a7ad007644e16b011db7b
unknown <scott.kumor1212@gmail.com>
2020-11-17 17:48:51 -0600

    products section and cart implemented

--- src/Pages/Home-Page/index.js
+++ src/Pages/Home-Page/index.js
@@ -1,12 +1,20 @@
 import React, { Component } from "react";
 import Hero from './../../Components/Hero-Image/index';
+import Products from './../../Components/Product-Section/index';
+import Cart from './../../Components/Cart/index';
+
+
 import "./styles.css";
 
 
 export default class Home extends Component {
   render() {
     return (
-    <Hero />      
+    <>
+      <Cart />
+      <Hero />
+      <Products />
+    </>
     );
   }
 }
