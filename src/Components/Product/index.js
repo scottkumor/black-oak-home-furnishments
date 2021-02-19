2e96fa1b95aa92b808cd6fcb7ee2905c8b2add08
Scott Kumor <scott.kumor1212@gmail.com>
2021-02-19 15:20:16 -0600

    product detail modal

--- src/Components/Product/index.js
+++ src/Components/Product/index.js
@@ -1,26 +1,77 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCartPlus } from '@fortawesome/free-solid-svg-icons'
-import "./style.css"
+import { faArrowRight, faCartPlus } from '@fortawesome/free-solid-svg-icons'
+import {Link} from "react-router-dom";
+import "./styles.css"
+import { Modal } from 'react-responsive-modal';
+import 'react-responsive-modal/styles.css';
+
+
 
 function Product(props) {
+    const [open, setOpen] = useState(false);
+
+    const onOpenModal = () => setOpen(true);
+    const onCloseModal = () => setOpen(false);
+
+    const myRef = React.useRef(null);
+
+
     return (
-        <article className="product" key={props.id} data-type={props.type}>
-            <div className="img-container">
+        <>
+            <div ref={myRef} className="reference"/>
+            <article className="product" key={props.id} data-type={props.type}>
+                <div className="img-container">
+                    <img src={process.env.PUBLIC_URL + `${props.image}`} alt={props.title} className="product-img" />
+                    <button className="bag-btn" data-id="1">
+                        <FontAwesomeIcon icon={faCartPlus} />
+                        <div> Add to Cart </div>
+                    </button>
+                    <button onClick={onOpenModal} className="modalBtn">View Details</button>
+                </div>
+                {/* <button className="bag-btn" data-id="1">
+                    <FontAwesomeIcon icon={faCartPlus} />
+                    Add to Cart
+                </button> */}
+                <h3>{props.title}</h3>
+                <h3>{props.category}</h3>
+                <h4>{props.collection}</h4>
+                <h4>{props.price}</h4>
+                {/* add one for type that comes in as an icon */}
+
+                {/* <p> {props.description}</p> */}
+
+            </article>
+        
+            <Modal open={open} onClose={onCloseModal} center container={myRef.current} focusTrapped={false}>
                 <img src={process.env.PUBLIC_URL + `${props.image}`} alt={props.title} className="product-img" />
-            </div>
-            {/* <button className="bag-btn" data-id="1">
-                <FontAwesomeIcon icon={faCartPlus} />
-                Add to Cart
-            </button> */}
-            <h3>{props.title}</h3>
-            <h3>{props.category}</h3>
-            <h4>{props.set}</h4>
-            <h4>{props.price}</h4>
-            {/* add one for type that comes in as an icon */}
-            
-            {/* <p> {props.description}</p> */}
-        </article>
+                <div className="modalContainer">
+                    <h1 className="modalTitle">{props.title}</h1>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+                        pulvinar risus non risus hendrerit venenatis. Pellentesque sit amet
+                        hendrerit risus, sed porttitor quam.
+                        
+                        {/* <p> {props.description}</p> */}
+                    </p>
+
+
+                    <div className="modalDetails">
+                        <h1>{props.price}</h1>
+
+                        <button className="modalCartBtn" data-id="1">
+                            <FontAwesomeIcon icon={faCartPlus} />
+                            <div> Add to Cart </div>
+                        </button>
+                        <Link className="modalLinkBtn" to="/">
+                            <p>See the entire &nbsp; </p> 
+                            <div>{props.collection}</div>
+                            <FontAwesomeIcon icon={faArrowRight}/>
+                        </Link>
+                    </div>
+                </div>
+            </Modal>
+        </>
     )
 }
 
--- src/Pages/Home-Page/index.js
+++ src/Pages/Home-Page/index.js
@@ -1,24 +1,55 @@
 import React, { Component } from "react";
+import "./styles.css";
+import Product from "./../../Components/Product"
+import ProductsDB from "./../../products.json"
 import Hero from './../../Components/Hero-Image/index';
-import Products from './../../Components/Product-Section/index';
+import Header from "./../../Components/Section-Header/index"
 
 
 import "./styles.css";
 
 
 export default class Home extends Component {
-  
+
   render() {
     return (
-    <>
-      <Hero 
-        tag="home"
-        heading="Furniture Selection"
-        subHeading="Browse by Collection"
-        link="/collections"
-      />
-      <Products />
-    </>
+      <>
+        <Hero
+          tag="home"
+          heading="Furniture Selection"
+          subHeading="Browse by Collection"
+          link="/collections"
+        />
+        <Header 
+          header="Our Products"
+          description="
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+          nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in 
+          culpa qui officia deserunt mollit anim id est laborum."
+        />
+        
+        <div className="products-center">
+          {ProductsDB.map(item => (
+            <Product
+              id={item.id}
+              key={item.id}
+              title={item.title}
+              price={"$" + item.price}
+              image={item.image}
+              category={item.category}
+              collection={item.collection}
+              type={item.type}
+            // description={item.description} short desc
+            // link={item.link} link to its own modal? page?
+            // icon={item.icon} icon based on category
+            />
+          ))}
+        </div>
+      </>
     );
   }
 }
