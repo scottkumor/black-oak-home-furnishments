776f5381340d0d742982b3c274b7397845d0a6ae
Scott Kumor <scott.kumor1212@gmail.com>
2021-10-11 14:18:35 -0500

    added cart icon updating when user adds unique item to cart

--- src/Components/Navigation-Bar/index.js
+++ src/Components/Navigation-Bar/index.js
@@ -1,107 +1,99 @@
-import React, { Component } from "react";
+import React, { useState, useEffect, useLayoutEffect } from "react";
 import { Link } from "react-router-dom";
 import "./styles.css"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars, faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import Cart from './../Cart/index';
 import Menu from './../Menu/index';
-import { cartHandler } from "./../../CartHelpers.js"
+import { lsCart, iconHandler } from "./../../CartHelpers.js"
 
 
+export default function NavBar() {
 
+  const [myIcon, setIcon] = useState([Object.keys(JSON.parse(lsCart).length)]);
+  const [cartView, setCartView] = useState(false);
+  const [menuView, setMenuView] = useState(false);
 
-export default class NavBar extends Component {
+  useEffect(() => {
 
-  
-  constructor(props) {
-    super(props);
-    this.state = {
-      cartView: false,
-      menu: false
-    };
+    /*
+    using an interval to update the icon in here rather than passing state back and forth.
+    re-rendering based on a child component that was not already rendered proved near impossible.
+    using code to check lsCart every time it changed did not work until the cart rendered. this
+    way i can passively check for updates without rendering the cart.
+    */
 
-    this.handleCart = this.handleCart.bind(this);
-    this.handleMenu = this.handleMenu.bind(this);
-
-  }
-
-
-  handleCart() {
-    this.setState(state => ({
-      cartView: !state.cartView
-    }));
-
-    if (this.state.cartView === false) {
-      this.disableScrolling()
-    }
-    else {
-      this.enableScrolling()
-    }
-  }
-
-  handleMenu() {
-    this.setState(state => ({
-      menu: !state.menu
-    }))
-    if (this.state.menu === false) {
-      this.disableScrolling()
-    }
-    else {
-      this.enableScrolling()
-    }
-  }
-
-  disableScrolling() {
-    var x = window.scrollX;
-    var y = window.scrollY;
-    window.onscroll = function () { window.scrollTo(x, y); };
-  }
-
-  enableScrolling() {
-    window.onscroll = null;
-  }
-
-  render() {
-
-    return (
-      <>
-        <nav className="navbar">
-          <div className="navbar-center">
-            <div className="menu-btn">
-              <span className="nav-icon">
-                <FontAwesomeIcon
-                  icon={faBars}
-                  size="2x"
-                  onClick={this.handleMenu}
-                />
-              </span>
-            </div>
-            {/* <img src="./../../images/logo.svg" alt="store logo" /> */}
-            <Link to="/">
-              <div> Black Oak </div>
-              <div>Home Furnishments</div>
-            </Link>
-            <div className="cart-btn">
-              <span className="nav-icon" onClick={this.handleCart}>
-                <FontAwesomeIcon
-                  icon={faShoppingCart}
-                  size="2x"
-                />
-                <div className="cart-items">{}</div>
-              </span>
-            </div>
-          </div>
-        </nav>
-
-        <Menu
-          menu={this.state.menu}
-          handleMenu={this.handleMenu}
-        />
-        <Cart
-          cartView={this.state.cartView}
-          handleCart={this.handleCart}
-        />
-      </>
+    const interval = setInterval(() =>
+      setIcon(iconHandler(myIcon)), 1000
     );
-  }
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (cartView === true || menuView === true) {
+      var x = window.scrollX;
+      var y = window.scrollY;
+      window.onscroll = function () { window.scrollTo(x, y); };
+    }
+    else {
+      window.onscroll = null;
+    }
+  }, [cartView, menuView]);
+
+
+  const handleCart = () => { setCartView(!cartView); }
+
+  const handleMenu = () => { setMenuView(!menuView); }
+
+  // const handleIcon = () => {
+  //   setIcon(iconHandler(myIcon));
+  // }
+
+  return (
+    <>
+      <nav className="navbar">
+        <div className="navbar-center">
+          <div className="menu-btn">
+            <span className="nav-icon">
+              <FontAwesomeIcon
+                icon={faBars}
+                size="2x"
+                onClick={() => handleMenu()}
+              />
+            </span>
+          </div>
+          {/* <img src="./../../images/logo.svg" alt="store logo" /> */}
+          <Link to="/">
+            <div> Black Oak </div>
+            <div>Home Furnishments</div>
+          </Link>
+          <div className="cart-btn">
+            <span className="nav-icon"
+              onClick={() => handleCart()}
+            >
+              <FontAwesomeIcon
+                icon={faShoppingCart}
+                size="2x"
+              />
+              <div className="cart-items">
+                {myIcon}
+              </div>
+            </span>
+          </div>
+        </div>
+      </nav>
+
+      <Menu
+        menu={menuView}
+        handleMenu={() => handleMenu()}
+      />
+      <Cart
+        cartView={cartView}
+        handleCart={() => handleCart()}
+        cartIcon={myIcon}
+      />
+    </>
+  );
 }
